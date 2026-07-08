ee1d0cb9ad1374dfd404d0317cd4435858a47021
v <261508724+vnoblitt@users.noreply.github.com>
2026-07-08 18:56:25 -0500

    Fixed sizing issues by making gridBoxes use border-box sizing. Added size limit of 100.
    Still need to fix the bug with the pointer going
    offscreen and releasing not causing the paint
    to be set to false.

--- sketch.js
+++ sketch.js
@@ -1,16 +1,51 @@
 /* sketch.js */
 
 const container = document.querySelector(".container");
-const clearButton = document.querySelector("button");
-const DEFAULT_SIZE = 16;
+const clearButton = document.getElementById("clear");
+const resizeButton = document.getElementById("resize");
+
+const CONTAINER_SIZE = 642;
+const DEFAULT_BOXES = 16;
+const DEFAULT_PIXEL_SIZE = resizePixel(DEFAULT_BOXES);
+
+let boxes = DEFAULT_BOXES;
+let pixelSize = DEFAULT_PIXEL_SIZE;
 
 clearButton.addEventListener("click", () => {
-    clearGrid(DEFAULT_SIZE);
+    clearGrid(boxes, boxes);
 })
 
-function clearGrid(gridSize) {
+resizeButton.addEventListener("click", () => {
+    let newSize = prompt("New size: ");
     
-    for (let i = 0; i < gridSize; i++) {
+    if (newSize > 100) {
+        console.log(newSize);
+        newSize = resizeReprompt();
+    }
+    console.log(newSize);
+    let oldSize = boxes;
+    boxes = newSize;
+    pixelSize = resizePixel(boxes);
+    clearGrid(boxes, oldSize);
+})
+
+function resizeReprompt(){
+    let newSize = prompt("Please enter a size less than 100: ")
+    if (newSize > 100) {
+        return resizeReprompt();
+    } else {
+        return newSize;
+    }
+}
+
+function resizePixel(boxes) {
+    let str = `${(container.offsetWidth - 2) / boxes}px`;
+    return str;
+}
+
+function clearGrid(gridSize, oldSize) {
+    
+    for (let i = 0; i < oldSize; i++) {
         const row = document.querySelector(".row");
         container.removeChild(row);
     }
@@ -27,13 +62,16 @@ function createGrid(gridSize) {
         for (let j = 0; j < gridSize; j++) {
             const gridBox = document.createElement('div');
             gridBox.classList.add('gridbox');
-            gridBox.addEventListener("mousedown", () => {
+            gridBox.style.width = pixelSize;
+            gridBox.style.height =  pixelSize;
+            gridBox.addEventListener("pointerdown", () => {
                 paint = true;
+                gridBox.style.backgroundColor = "black";
             })
-            gridBox.addEventListener("mouseup", () => {
+            gridBox.addEventListener("pointerup", () => {
                 paint = false;
             })
-            gridBox.addEventListener("mouseenter", () => {
+            gridBox.addEventListener("pointerenter", () => {
                 if (paint) {
                     gridBox.style.backgroundColor = "black";
                 }
@@ -43,4 +81,4 @@ function createGrid(gridSize) {
     }
 }
 
-createGrid(16);
+createGrid(boxes);
